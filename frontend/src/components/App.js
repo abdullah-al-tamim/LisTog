aaef7da1812f458551c3440e88c102808bd4d0e0
Abdullah al Tamim <2020-1-60-127@std.ewubd.edu>
2024-06-24 12:30:03 +0600

    model is up to date

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,20 +1,25 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 import HomePage from "./HomePage";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
+    
   }
 
   render() {
     return (
-      <div>
-        <HomePage />
-      </div>
+    <div>
+      <HomePage/>
+      <RoomJoinPage/>
+      <CreateRoomPage/>
+    </div>
     );
   }
 }
 
 const appDiv = document.getElementById("app");
-render(<App name = "tamim"/>, appDiv);
+render(<App />, appDiv);
